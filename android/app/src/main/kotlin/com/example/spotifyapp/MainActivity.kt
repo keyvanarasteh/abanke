39de52fa1eab1b2326b76da5e762a3167db4e3e4
ismet efe balık <efebalikofc@hotmail.com>
2023-01-03 19:36:38 +0300

    some updates on home

---
+++ android/app/src/main/kotlin/com/example/spotifyapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spotifyapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, file_names, prefer_const_literals_to_create_immutables, prefer_final_fields
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,11 +10,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _selectedIndex = 0;
+  void _navigateBottomNavBar(int index)
+  {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _children = [
+    Center(child: Text('HOME')),
+    Center(child: Text('SEARCH')),
+    Center(child: Text('VIDEOS')),
+    Center(child: Text('SHOP')),
+    Center(child: Text('ACCOUNT')),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("home "),
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavBar,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_call), label: 'videos'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account'),
+        ],
       ),
     );
   }
